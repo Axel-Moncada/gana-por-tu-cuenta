99d1fb0ca09e20aefd54274b552b1fe4780589f6
Axel Moncada Desktop <axelemoncada@gmail.com>
2024-07-12 10:28:25 -0500

    Ajuste abstrac TYC

--- src/components/ComoParticipar.jsx
+++ src/components/ComoParticipar.jsx
@@ -171,7 +171,10 @@ export const ComoParticipar = () => {
                 </li>
                 <li className="textoOportunidades">Presentar documento de identidad.</li>
               </ul>
+
+             
             </div>
+         
           </div>
           <br /> <br />
           {/*<button className="btnConoce">Conoce más aquí</button>*/}
@@ -180,6 +183,11 @@ export const ComoParticipar = () => {
 
           </span>
           <div id="containerSpace"></div>
+          <p className="tycp">
+              Aplican Términos y Condiciones 
+
+ABSTRACT:  I. Nombre y razón social: Banco Unión S.A. NIT 860006797-9 – II. Nombre del Juego Promocional: Con Banco Unión Ganas por tu Cuenta – III. Quiénes pueden participar: en esta actividad pueden participar clientes personas naturales, mayores de edad, con domicilio en Colombia, que tengan Cuenta de Ahorros BANCO UNIÓN S.A. en estado activas y vigentes a la fecha de cada corte. No participan empleados de la compañía vinculados directamente o a través de temporal, ni los accionistas ni los vinculados de los accionistas, ni las cuentas de ahorro abiertas en las oficinas 201 o 76.  Los clientes participan de forma independiente con cada una de sus cuentas de ahorro. – IV. Vigencia del Juego Promocional: desde las 00:00 del día veinte (20) de agosto de 2024 hasta las 11:59 del día veinte (20) de diciembre de 2024 – V. Mecánica del Juego Promocional: un cliente tendrá la oportunidad de ganar por cada CINCUENTA MIL PESOS M/CTE ($50.000) que aumenten el saldo de su cuenta de ahorros. (el cálculo de oportunidades para participar en el sorteo por este ítem será: se tomará en cuenta el saldo de cada una de las cuentas de ahorros participantes en la promoción a la fecha de corte final y se establecerá la diferencia con el saldo a la fecha de corte inicial.  Esa diferencia, solo en los casos en que sea positiva, se dividirá por $50.000 y el numero resultante de la división corresponderá a la cantidad de oportunidades; sólo se tomará en cuenta el número entero resultante sin decimales), y /o por Cada Giro WESTERN UNION abonado a la Cuenta de Ahorros de BANCO UNIÓN S.A., a través de canales digitales o la red de oficinas a nivel nacional, el cálculo de oportunidades para participar en el sorteo por este ítem será: Se asignará una oportunidad a cada cliente por cada giro WESTERN UNION abonado en la cuenta de ahorros de BANCO UNIÓN S.A. de la que es titular, a través de los canales no presenciales y/o digitales y a través de la red de oficinas de Banco Unión a nivel nacional durante el corte respectivo de cada uno de los sorteos. Entiéndase por Canales digitales y/o no presenciales, portal transaccional www.bancounion.com , APP BANCO UNION, Contact Center 018000111999 y giro directo a la cuenta de ahorros de Banco Unión S.A. NIT 860006797-9 – VI. Fechas de Sorteos, hora y lugar donde se llevarán a cabo: los sorteos se llevarán a cabo en la Dirección General de BANCO UNIÓN S.A. NIT 860006797-9, ubicada en la Calle 4 # 27 – 52, barrio San Fernando, Santiago de Cali, el ocho (8) de octubre de 2024 a las 2:00 p.m., y el veintitrés (23) de diciembre de 2024 a las 10:30 a.m. - VII. Plan de Premios: Dos (2) Bonos de viaje, por valor de cinco millones de pesos M/CTE ($5.000.000) cada uno y un (1) carro NISSAN MARCH 1.6L "SENSE CONNECT" MEC D.ELECT AA RIM 15" LUJO 2A/bag B.C V.E RADIO TOUCH SCREEN MANOS LIBRES AUDIO TIMON EXPLORADORAS CAMARA REVERSA, MODELO 2023; premios que se entregarán a través de tres (03) sorteos. Los clientes participantes tendrán la oportunidad de ser uno de los tres (3) posibles ganadores de los tres (3) premios  - VIII Cobertura del Juego Promocional: Aplica únicamente en territorio colombiano para clientes con Cuenta de Ahorros Banco Unión S.A. NIT 860006797-9., en estado activas durante la vigencia de este juego promocional.  
+              </p>
         </div>
       </div>
 
